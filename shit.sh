3292f188cfdb8fd6f6c3f1936de379699ae2ad7e
LucasZHN <shibaketsu@foxmail.com>
2021-12-29 17:17:37 +0800

    test

--- shit.sh
+++ shit.sh
@@ -3,5 +3,5 @@ git init
 git add .
 git commit -m "$1"
 git branch -M main
-git remote add origin git@github.com:LucasZHN/Lucas-s-Shit.git
+#git remote add origin git@github.com:LucasZHN/Lucas-s-Shit.git
 git push -u origin main
