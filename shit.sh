b9956ef4b092de994b3d3d5e1b2e8206412aa799
LucasZHN <shibaketsu@foxmail.com>
2021-12-29 17:17:02 +0800

    test

--- shit.sh
+++ shit.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo "# Lucas-s-Shit" >> README.md
 git init
-git add README.md
-git commit -m "first commit"
+git add .
+git commit -m "$1"
 git branch -M main
 git remote add origin git@github.com:LucasZHN/Lucas-s-Shit.git
 git push -u origin main
